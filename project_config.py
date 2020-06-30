2af96f7e8b73052015481a1690b851a4f86f43bb
HHansi <h.hansi1991@gmail.com>
2020-06-30 17:34:28 +0100

    001: Init

---
+++ project_config.py
@@ -0,0 +1,4 @@
+# Created by Hansi at 6/30/2020
+SEED = 77
+TRAINING_DATA_PATH = "E:/Work Spaces/Python/W-NUT/COVID19Tweet/train.tsv"
+VALIDATION_DATA_PATH = "E:/Work Spaces/Python/W-NUT/COVID19Tweet/valid.tsv"
---
+++ util/logginghandler.py
@@ -0,0 +1,18 @@
+import logging
+
+import tqdm
+
+
+class TQDMLoggingHandler(logging.Handler):
+    def __init__(self, level=logging.NOTSET):
+        super().__init__(level)
+
+    def emit(self, record):
+        try:
+            msg = self.format(record)
+            tqdm.tqdm.write(msg)
+            self.flush()
+        except (KeyboardInterrupt, SystemExit):
+            raise
+        except:
+            self.handleError(record)
