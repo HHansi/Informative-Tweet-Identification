3e5a3f17ad5e62fa074806bc4f9dd967ef8a076a
HHansi <h.hansi1991@gmail.com>
2020-07-03 17:37:44 +0100

    005: Updated project configs

--- project_config.py
+++ project_config.py
@@ -1,4 +1,17 @@
 # Created by Hansi at 6/30/2020
 SEED = 77
+
+LABELS = 'labels'
+POS_LABEL = 'positive_label'
+CONFUSION_MATRIX = 'confusion_matrix'
+ACCURACY = 'accuracy'
+F1 = 'f1'
+RECALL = 'recall'
+PRECISION = 'precision'
+
 TRAINING_DATA_PATH = "E:/Work Spaces/Python/W-NUT/COVID19Tweet/train.tsv"
 VALIDATION_DATA_PATH = "E:/Work Spaces/Python/W-NUT/COVID19Tweet/valid.tsv"
+
+# TRAINING_DATA_PATH = "/content/drive/My Drive/Colab Notebooks/W-NUT/COVID19Tweet/train.tsv"
+# VALIDATION_DATA_PATH = "/content/drive/My Drive/Colab Notebooks/W-NUT/COVID19Tweet/valid.tsv"
+
