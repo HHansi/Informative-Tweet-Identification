8fac818f7a7ab888e960c87383339dcd1c9c81bf
TharinduDR <rhtdranasinghe@gmail.com>
2020-07-17 16:53:48 +0100

    010: Upgrading the transformers version

--- algo/neural_nets/models/transformers/language_modeling/language_modeling_model.py
+++ algo/neural_nets/models/transformers/language_modeling/language_modeling_model.py
@@ -4,11 +4,13 @@
 
 from __future__ import absolute_import, division, print_function
 
+import glob
 import json
 import logging
 import math
 import os
 import random
+import shutil
 import warnings
 from multiprocessing import cpu_count
 from typing import Dict, List
@@ -603,6 +605,12 @@ class LanguageModelingModel:
                             )
 
                     if args.save_steps > 0 and global_step % args.save_steps == 0:
+
+                        if args.save_recent_only:
+                            del_paths = glob.glob(os.path.join(output_dir, 'checkpoint-*'))
+                            for del_path in del_paths:
+                                shutil.rmtree(del_path)
+
                         # Save model checkpoint
                         output_dir_current = os.path.join(output_dir, "checkpoint-{}".format(global_step))
 
@@ -623,6 +631,11 @@ class LanguageModelingModel:
                         if self.is_world_master():
                             for key, value in results.items():
                                 tb_writer.add_scalar("eval_{}".format(key), value, global_step)
+
+                        if args.save_recent_only:
+                            del_paths = glob.glob(os.path.join(output_dir, 'checkpoint-*'))
+                            for del_path in del_paths:
+                                shutil.rmtree(del_path)
 
                         output_dir_current = os.path.join(output_dir, "checkpoint-{}".format(global_step))
 
@@ -691,6 +704,12 @@ class LanguageModelingModel:
                     return global_step, tr_loss / global_step
 
             epoch_number += 1
+
+            if args.save_recent_only:
+                del_paths = glob.glob(os.path.join(output_dir, 'checkpoint-*'))
+                for del_path in del_paths:
+                    shutil.rmtree(del_path)
+
             output_dir_current = os.path.join(output_dir, "checkpoint-{}-epoch-{}".format(global_step, epoch_number))
 
             if args.save_model_every_epoch or args.evaluate_during_training:
