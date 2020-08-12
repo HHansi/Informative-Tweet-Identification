4c4d6a5bcccf3d180d342c9c99c9b6e40fc5e5a4
HHansi <h.hansi1991@gmail.com>
2020-08-12 17:26:48 +0100

    013: Added emoji to text

--- algo/neural_nets/common/preprocessor.py
+++ algo/neural_nets/common/preprocessor.py
@@ -1,9 +1,21 @@
+import demoji
+demoji.download_codes()
+
+
 def remove_words(x):
     x = x.replace('@USER', '')
     x = x.replace('HTTPURL', '')
     return x
 
 
+def add_emoji_text(x):
+    emoji_text = demoji.findall(x)
+    for em in emoji_text.keys():
+        x = x.replace(em, " " + emoji_text[em] + " ")
+    return x
+
+
 def transformer_pipeline(x):
     x = remove_words(x)
+    x = add_emoji_text(x)
     return x
