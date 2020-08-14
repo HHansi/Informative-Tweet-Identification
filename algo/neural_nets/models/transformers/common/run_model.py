29027317a239adefb652757095e2e0a980e273a1
HHansi <h.hansi1991@gmail.com>
2020-08-14 20:01:16 +0100

    016: Updated experiment args

--- algo/neural_nets/models/transformers/common/run_model.py
+++ algo/neural_nets/models/transformers/common/run_model.py
@@ -84,7 +84,7 @@ class ClassificationModel:
             "electra": (ElectraConfig, ElectraForSequenceClassification, ElectraTokenizer),
             "roberta": (RobertaConfig, RobertaForSequenceClassification, RobertaTokenizer),
             "albert": (AlbertConfig, AlbertForSequenceClassification, AlbertTokenizer),
-            "bertweet": (RobertaConfig, AutoModelForSequenceClassification, RobertaTokenizer),
+            "bertweet": (BertConfig, AutoModelForSequenceClassification, BertTokenizer),
         }
 
         self.args = self._load_model_args(model_name)
